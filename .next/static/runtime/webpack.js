67575c1eaeec4b9d11b342a84a76be051894eb8e
amal jose <amal.jose@appsintegra.com>
2020-04-03 10:10:13 +0530

    Add more files

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2eb31dabaa2e6bbf3619";
+/******/ 	var hotCurrentHash = "71cf0f32543828b551f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
